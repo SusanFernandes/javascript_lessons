a850318ec5697ab0433d21130b8181e71c6fe87d
SusanFernandes <112262940+SusanFernandes@users.noreply.github.com>
2023-12-10 06:53:54 +0000

    dataatypes and some info

---
+++ basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //means use all javascript codes as the newer version
+// alert(3+3) way of generting alert in a web console,  * we are using node js not browser
+
+//ecma script tc39.es this is the original documentation of javascript and mdn can also be referred but that is  produced by microsoft 
+
+/* 
+data types in js:
+> number
+> bigInt => big numbers used in facebook, crypto , stocks etc
+> string =>"Susan"
+> boolean => true/false
+> null => standalone value =>but is an object datatype =>null means the value is empty. it is not undefined
+> undefined => is a datatype itself
+> symbol =>unique => mostly used in react to determine the uniqueness of the components also defines the differences in the components
+> objects
+
+console.log(typeof "Susan")
+let age= 3
+console.log(typeof age) : gives you answer as number since the value stoerd in the age variable is a number
+
+*/
+let name ="Susan"
+let age =19
+let female=true
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof female)
+console.log(typeof null)
