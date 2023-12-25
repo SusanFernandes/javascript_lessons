30003a73e18da050aa33daaa8ae7555a4d1520f0
SusanFernandes <112262940+SusanFernandes@users.noreply.github.com>
2023-12-25 06:28:42 +0000

    lesson done

--- basics/03_conversionsAndOperations.js
+++ basics/03_conversionsAndOperations.js
@@ -23,3 +23,28 @@ let nameString="33abc"
 console.log(typeof nameString)
 let nameNumber = Number(nameString)
 console.log(typeof nameNumber)
+console.log(nameNumber)
+// NaN = Not a Number since 33abc is not a number . NaN is also a type.
+
+let aNull = null
+console.log(aNull)
+console.log(typeof aNull)
+let numberNull = Number(aNull)
+console.log(numberNull) // null value is 0 when convert in number
+
+let valueUndefined= undefined
+console.log(typeof valueUndefined)
+let thisUndefined = Number(valueUndefined)
+console.log(thisUndefined)
+
+let boolValue = 1;
+console.log(typeof boolValue)
+let booleanValue = Boolean(boolValue)
+console.log(booleanValue)
+
+/*
+ 1 => true; 
+ 0=> false;
+ "" = false ;
+ "ValueInTheString"= true
+*/
