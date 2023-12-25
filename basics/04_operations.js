1b77acab0f38595ff4fe204012b13a4df0369caa
SusanFernandes <112262940+SusanFernandes@users.noreply.github.com>
2023-12-25 07:55:39 +0000

    operations and conversions

---
+++ basics/04_operations.js
@@ -0,0 +1,28 @@
+let value= 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2) // addition
+console.log(3-2) //subtraction
+console.log(2*2) //multiplication
+console.log(2**3) // exponential 2 raised to 3 = 2^3 = 8
+console.log(2/3) // divide gives quotient as an answer
+console.log(2%3)  // modulo gives remainder as the answer
+let str1 = "Hi,"
+let str2= "Susan here"
+let str3= str1 +" "+str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1+"2") 
+console.log("1"+2+2) //first only it is converted in string so the whole thing is a string
+console.log(1+2+"2") //first number addition then string is added
+console.log(1+"2"+2) // first number addition then comes a string which converts everything behind it in a string addition thing
+//if string is first then everything following will be string addition unless parentheses are added ig. and if string is behind or in between then whatever operation is there in front of it is carried and then string follows
+
+// while comparing two datatypes you should keep both datatypes same
+// null == 0 : false null>=0 : true  null>0 : false
+//undefined is always false in comparisons like above null
+
+
+// strict check === checks datatype and value
